d2fd5a35061d61b5363f80886f86d332892e400f
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2019-07-23 18:21:34 +1000

    prettier

--- src/lib/FileDownload.ts
+++ src/lib/FileDownload.ts
@@ -1,20 +1,20 @@
-import AWS from "aws-sdk";
-import fs from "fs-extra";
+import AWS from 'aws-sdk'
+import fs from 'fs-extra'
 
 interface FileDownloadInterface {
-  start: () => Promise<void>;
+  start: () => Promise<void>
   getStats: () => {
-    bytesLoaded: number;
-    progress: number;
-    totalSize: number;
-  };
+    bytesLoaded: number
+    progress: number
+    totalSize: number
+  }
 }
 
 export default class FileDownload implements FileDownloadInterface {
-  private s3Object: AWS.S3.Object;
-  private destinationPrefix: string;
-  private downloadRequest: AWS.Request<AWS.S3.GetObjectOutput, AWS.AWSError>;
-  private bytesLoaded = 0;
+  private s3Object: AWS.S3.Object
+  private destinationPrefix: string
+  private downloadRequest: AWS.Request<AWS.S3.GetObjectOutput, AWS.AWSError>
+  private bytesLoaded = 0
 
   public constructor(
     s3: AWS.S3,
@@ -22,36 +22,36 @@ export default class FileDownload implements FileDownloadInterface {
     bucketName: string,
     destinationPrefix: string
   ) {
-    this.s3Object = s3Object;
-    this.destinationPrefix = destinationPrefix;
+    this.s3Object = s3Object
+    this.destinationPrefix = destinationPrefix
     this.downloadRequest = s3.getObject({
       Bucket: bucketName,
       Key: s3Object.Key
-    });
+    })
   }
 
   public start = async (): Promise<void> => {
-    const filePath = this.s3Object.Key;
-    const directoryPath = filePath.substring(0, filePath.lastIndexOf("/"));
+    const filePath = this.s3Object.Key
+    const directoryPath = filePath.substring(0, filePath.lastIndexOf('/'))
     // create folder structure if it doesn't exist
-    await fs.ensureDir(this.destinationPrefix + directoryPath);
+    await fs.ensureDir(this.destinationPrefix + directoryPath)
 
-    const stream = this.downloadRequest.createReadStream();
+    const stream = this.downloadRequest.createReadStream()
 
-    stream.on("data", chunk => {
-      this.bytesLoaded += chunk.length;
-    });
+    stream.on('data', (chunk) => {
+      this.bytesLoaded += chunk.length
+    })
 
-    stream.pipe(fs.createWriteStream(this.destinationPrefix + filePath));
+    stream.pipe(fs.createWriteStream(this.destinationPrefix + filePath))
 
-    return new Promise(resolve => {
-      stream.on("finish", () => resolve());
-    });
-  };
+    return new Promise((resolve) => {
+      stream.on('finish', () => resolve())
+    })
+  }
 
   public getStats = () => ({
     bytesLoaded: this.bytesLoaded,
     progress: (this.bytesLoaded / this.s3Object.Size) * 100,
     totalSize: this.s3Object.Size
-  });
+  })
 }

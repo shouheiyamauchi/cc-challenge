094f3af8b194d4ba76864b69debcf92268d74b3c
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2019-07-25 16:02:47 +1000

    remove debug code

--- src/start.ts
+++ src/start.ts
@@ -1,12 +1,3 @@
-import fs from 'fs-extra'
-
 import { start } from './commands'
-
-const hello = async () => {
-  console.log(await fs.pathExists('./downloads/yes'))
-  console.log(await fs.pathExists('./downloads/www.breakinotes.com'))
-}
-
-// hello()
 
 start()
